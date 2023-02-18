3ae45d5ed7becbe187117eb0ef82d07f67838df4
Kostya Sergienko <sergienko.kostya@ukr.net>
2023-02-18 21:39:43 +0200

    s

--- modal.js
+++ modal.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[booking-modal-open]"),
-    closeModalBtn: document.querySelector("[booking-modal-close]"),
-    modal: document.querySelector("[booking-modal]"),
+    openModalBtn: document.querySelector("[price-modal-open]"),
+    closeModalBtn: document.querySelector("[price-modal-close]"),
+    modal: document.querySelector("[price-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-booking-hidden");
+    refs.modal.classList.toggle("is-price-hidden");
   }
 })();
